d96706f90995d4de2a5b1066ae8b2066895c6728
Tobias Fürtjes <tobias.fuertjes@hpe.com>
2016-10-13 08:50:49 +0200

    Added mongoose.js

---
+++ node/Test2/config/mongoose.js
@@ -0,0 +1,10 @@
+var config = require('./config'),
+    mongoose = require('mongoose');
+
+module.exports = function() {
+    //Solved warning with next line  -> Also error that some parts of DB are missing
+    mongoose.Promise = global.Promise;
+    var db = mongoose.connect(config.db);
+    require('../app/models/link.server.model');
+    return db;
+};
