6e5593e01d38923888abcedddd0fec6e8e235628
kai-oswald <kaos1910@googlemail.com>
2016-10-13 14:27:59 +0200

    integrate frontend to node controller

--- node/public/js/app.js
+++ node/public/js/app.js
@@ -3,12 +3,10 @@ $(document).ready(function() {
         $(this).html('<i class="fa fa-refresh fa-spin"></i>');
         // send login information to server and validate
         // success: log user in, disable VIP Code input
-        // failure: show error message, reset inputs
-
-        // simulate login with 50% success-rate and delay of 1 sec
+        // failure: show error message, reset inputs        
         $(this).delay(1000).queue(function(n) {
-            var rnd = Math.random();
-            if (rnd > 0.5) {
+            var code = $(".login-form").val();
+            if (code !== "test") {
                 $(".login-form").prop("disabled", false);
                 $(this).html('<i class="fa fa-sign-in" aria-hidden="true"></i>');
             } else {
@@ -19,15 +17,27 @@ $(document).ready(function() {
         })
     });
 
+    // VIP CODE: listen for enter key press
+    $(".login-form").keypress(function(e) {
+      if (e.keyCode == 13)
+          $('.btn-login').click();
+    });
+
+    // make request to shorten link on button click
     $(".btn-shorten").click(function() {
         var url = $(".input-url").val();
         console.log(url);
+        var json = {
+          "shortlink": "bcs",
+          "longlink": url
+        }
         // validate url
         if (isURL(url)) {
             $.ajax({
                 url: "/link",
                 method: "POST",
-                data: url,
+                data: json,
+                dataType: "json",
                 success: function(res, status) {
                     if (res !== null) {
                         $(".result").html("Your shortened link: " + "<a href='" + res + "'>" + res + "</a>");
@@ -46,22 +56,27 @@ $(document).ready(function() {
 
     });
 
+    // listen for enter keypress
     $('.input-url').keypress(function(e) {
         if (e.keyCode == 13)
             $('.btn-shorten').click();
     });
-    var count = 0;
+    var count = 36187;
     setInterval(function() {
-        $(".count").html(count++);
+      var rnd = Math.floor(Math.random() * 100);
+      count+=rnd;
+        $(".count").html(count);
     }, 1000);
 });
 
+
+// validates a URL
 function isURL(str) {
-    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-    return pattern.test(str);
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return pattern.test(str);
 }
