89dae391cca312cc6c72d134afedd5523d496889
Lennart Gustafsson <l.gustafsson@web.de>
2016-10-18 11:50:57 +0200

    Removed /l linking. Removed view from link.server.controller.js. Added basic validation.

--- node/app/controllers/link.server.controller.js
+++ node/app/controllers/link.server.controller.js
@@ -1,17 +1,33 @@
 var Link = require('mongoose').model('Link');
-
-exports.render = function(req, res) {
-    res.render('main', {
-        title: 'Willkommen'
-    })
+var LinkError = {
+    shortlink: String,
+    longlink: String,
+    error: String
 };
 
 exports.create = function(req, res, next) {
     //TODO: Formatlong link into right format! Access about req.body.longlink...
     var link = new Link(req.body);
-    console.log(link.shortlink);    if (link.shortlink == "") {
+    console.log(link.shortlink);
+    //Check if cookie is set
+    //Check if url is valide url
+    if (validateUrl(link.longlink) == false) {
+        if (validateUrl("http://" + link.longlink) == true) {
+            link.longlink = "http://" + link.longlink;
+        } else {
+            //Throw Error
+            LinkError.shortlink = link.shortlink;
+            LinkError.longlink = link.longlink;
+            LinkError.error = 'URL malformatted';
+            res.json(LinkError);
+        }
+    }
+
+    if (link.shortlink == "") {
         link.shortlink = randomText();
     }
+
+    console.log(link);
     Link.find({
         "longlink": link.longlink
     }, function(err, docs) {
@@ -20,21 +36,23 @@ exports.create = function(req, res, next) {
         } else {
             console.log(docs);
             if (docs.length != 0) {
-                link.shortlink = "/l/" + docs[0].shortlink;
+                link.shortlink = "/" +docs[0].shortlink;
                 res.json(link);
             } else {
-    link.save(function(err) {
-        if (err) {
-            return next(err);
-        } else {
-            // TODO: temp fix
-            link.shortlink = "/l/" + link.shortlink;
-            res.json(link);
-        }
-    });
+                link.save(function(err) {
+                    if (err) {
+                        return next(err);
+                    } else {
+                        // TODO: temp fix
+                        link.shortlink = "/" + link.shortlink;
+                        res.json(link);
+                    }
+                });
             }
         }
     });
+    /*delete mongoose.models.Link;
+    delete mongoose.modelSchemas.Link;*/
 };
 
 exports.list = function(req, res, next) {
@@ -53,7 +71,7 @@ exports.text = function(req, res) {
 
 exports.redirect = function(req, res) {
     //Longlink has to save in standardformat to make this redirect correct
-    res.redirect("http://" + req.link.longlink);
+    res.redirect(req.link.longlink);
     //console.log("test");
 };
 
@@ -90,4 +108,8 @@ function randomText() {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
+}
+
+function validateUrl(value) {
+    return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(value);
 }
--- node/app/routes/link.server.routes.js
+++ node/app/routes/link.server.routes.js
@@ -4,15 +4,15 @@ var bodyParser = require('body-parser')
 module.exports = function(app) {
     app.use( bodyParser.json() );       // to support JSON-encoded bodies
 
-    app.route('/link').post(links.create).get(links.render);
-    
+    app.route('/link').post(links.create);
+
     //redirect
-    app.route('/l/:slink').get(links.redirect);
-    
+    app.route('/:slink').get(links.redirect);
+
     app.route('/all').get(links.list);
-    
+
     app.param('slink', links.linkByShort);
-    
+
     app.route('/linktext').get(links.text).post(links.text);
 
 };
