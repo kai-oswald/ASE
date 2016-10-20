7fc6426cef9506465b7df56722399ff6a5df634d
kai-oswald <kaos1910@googlemail.com>
2016-10-20 13:32:04 +0200

    add new global variable for admin and implement similiary to premium

--- node/app/controllers/admin.server.controller.js
+++ node/app/controllers/admin.server.controller.js
@@ -1,18 +1,24 @@
-var grantAccess = false;
 exports.render = function (req, res) {
-    // check if Admin is logged in and grant access
+    // check if Admin is logged in and grant access    
     res.render('admin', {
         title: 'Admin',
-        layout: 'layout',
-        isAdmin: grantAccess,
+        layout: 'layout'
     })
 };
 
 exports.validate = function (req, res) {
     if (req.body.password === "qwer") {
-        grantAccess = true;
-        res.json({ success: true });
+        res.cookie('admin', 'true', {
+            maxAge: 900000,
+            httpOnly: true
+        });
+        res.json({
+            success: true
+        });
     } else {
-        res.json({ success: false, err: "Invalid Password"});
+        res.json({
+            success: false,
+            err: "Invalid Password"
+        });
     }
 };
