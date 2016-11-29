ab9818795696d8851727a504967779bb39a16f6b
Marcel Lechler <marcel.lechler@hpe.com>
2016-11-29 14:36:28 +0100

    2 Tests

--- node/test/link.server.controller.js
+++ node/test/link.server.controller.js
@@ -1,11 +1,30 @@
-var expect    = require("chai").expect;
+var expect = require("chai").expect;
 var link = require("../app/controllers/link.server.controller.js");
+var httpMocks = require('node-mocks-http');
 
-describe("Link Controller", function() {
-  describe("Random Text Generator", function() {
-    it("creates random string", function() {
-      var rnd = link.randomText();
-      expect(rnd.length).to.equal(5);     
+describe("Link Controller", function () {
+    describe("Check Short Link", function () {
+        it("checks if shortlink already exists", function () {
+            GLOBAL_SERVER = 'localhost:8001';
+            GLOBAL_PREMIUM = true;
+            var req = httpMocks.createRequest({body: {shortlink: 'abc', longlink: 'www.abc.de'}});
+            var res = httpMocks.createResponse();
+            link.create(req, res);
+            link.checkShortLink(req, res);
+            expect(res.statusCode).to.equal(200);
+        });
     });
-  }); 
+});
+
+describe("Link Controller", function () {
+    describe("Check Short Link", function () {
+        it("checks if shortlink is a new one", function () {
+            GLOBAL_SERVER = 'localhost:8001';
+            GLOBAL_PREMIUM = true;
+            var req = httpMocks.createRequest({body: {shortlink: 'abc', longlink: 'www.abc.de'}});
+            var res = httpMocks.createResponse();
+            link.checkShortLink(req, res);
+            expect(res.statusCode).to.equal(200);
+        });
+    });
 });
