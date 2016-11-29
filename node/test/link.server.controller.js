016e72f7fc7373be4aa6be4ad4214784b01990b3
DTobii <tobi00@arcor.de>
2016-11-29 14:17:36 +0100

    3 Tests Need to install node-mocks-http !!

--- node/test/link.server.controller.js
+++ node/test/link.server.controller.js
@@ -1,5 +1,6 @@
 var expect    = require("chai").expect;
 var link = require("../app/controllers/link.server.controller.js");
+var httpMocks = require('node-mocks-http');
 
 describe("Link Controller", function() {
   describe("Random Text Generator", function() {
@@ -8,4 +9,40 @@ describe("Link Controller", function() {
       expect(rnd.length).to.equal(5);     
     });
   }); 
+    
+    describe("CheckLongLink test", function() {
+        it("Premium and Systemurl", function() {
+            var req = httpMocks.createRequest({body:{shortlink:'stats', longlink:'testt.de'}});
+            var res = httpMocks.createResponse();
+            GLOBAL_PREMIUM='true';
+            console.log(req);
+            var re = link.checkLongLink(req, res, function() {
+                expect(res.error).to.equal(200); //gesetzt?
+            });
+            
+        });
+    });   
+    describe("CheckLongLink test", function() {
+        it("Premium and no Systemurl", function() {
+            var req = httpMocks.createRequest({body:{shortlink:'abcd', longlink:'testt.de'}});
+            var res = httpMocks.createResponse();
+            GLOBAL_PREMIUM='true';
+            var re = link.checkLongLink(req, res, function() {
+                expect(res.statusCode).to.equal(200);
+            });
+            
+        });
+    });   
+    describe("CheckLongLink test", function() {
+        it("No Premium", function() {
+            var req = httpMocks.createRequest({body:{shortlink:'qweqe', longlink:'testt.de'}});
+            var res = httpMocks.createResponse();
+            GLOBAL_PREMIUM='false';
+            var re = link.checkLongLink(req, res, function() {
+                expect(res.statusCode).to.equal(200);
+            });
+            
+        });
+    });   
+    
 });
