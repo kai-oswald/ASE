16191498ef0478722a8e77f9b5a46ea3e17422a9
Marcel Lechler <marcel.lechler@hpe.com>
2016-12-09 10:53:14 +0100

    Updated Unit Test

--- node/test/index.server.controller.js
+++ node/test/index.server.controller.js
@@ -5,6 +5,7 @@ var index = require("../app/controllers/index.server.controller.js");
 describe("Index Controller", function () {
     describe("Check Title", function () {
         it("checks the title of the page", function () {
+            GLOBAL_PREMIUM = false;
             var req = httpMocks.createRequest();
             var res = httpMocks.createResponse();
             index.render(req, res);
